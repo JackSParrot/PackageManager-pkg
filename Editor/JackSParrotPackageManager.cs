abc18c7f70ab6205a7505ba645e6a8055ea80706
fran <fran@madbox.com>
2021-03-29 09:58:01 +0200

    fixed refresh issues

--- Editor/JackSParrotPackageManager.cs
+++ Editor/JackSParrotPackageManager.cs
@@ -1,182 +1,183 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.Networking;
 using System.Threading.Tasks;
 
-public class JackSParrotPackageManager : EditorWindow
+namespace JackSParrot.Editor
 {
-    class PackageData
+    public class JackSParrotPackageManager : EditorWindow
     {
-        public string Name = "Empty";
-        public string Url = "Empty";
-        public string RemoteHash = "EmptyRemote";
-        public string LocalHash = "EmptyLocal";
-        public bool Installed = false;
-    }
-
-    const string kUrl = "https://script.google.com/macros/s/AKfycbyTB22eh9Todbw66Xx6qk-pUb3jzBN8ysnPlaSwZESKFoYAGbE/exec";
-    const string kPrefsKey = "JACKSPARROT_PACKAGES";
-
-    List<PackageData> _packages = new List<PackageData>();
-    string _status = string.Empty;
-    bool _loading = true;
-
-    [MenuItem("JackSParrot/Packages")]
-    static void OpenWindow()
-    {
-        var window = (JackSParrotPackageManager)GetWindow(typeof(JackSParrotPackageManager));
-        window.Initialize();
-        window.Show();
-    }
-
-    void Initialize(bool forced = false)
-    {
-        _loading = true;
-        if (!EditorPrefs.HasKey(kPrefsKey) || forced)
+        private class PackageData
         {
-            _ = DownloadLatestConfig();
-        }
-        else
-        {
-            ParseData(EditorPrefs.GetString(kPrefsKey));
-        }
-    }
-
-    void ConfigDownloaded(UnityWebRequest r)
-    {
-        if (!string.IsNullOrEmpty(r.error))
-        {
-            _status = "Error: " + r.error;
-            Debug.LogError(r.error);
-            return;
-        }
-        string data = r.downloadHandler.text;
-        EditorPrefs.SetString(kPrefsKey, data);
-        ParseData(data);
-    }
-
-    void ParseData(string data)
-    {
-        _status = "Parsing received data";
-        _packages.Clear();
-        var lines = data.Split(';');
-        foreach (var line in lines)
-        {
-            var parts = line.Split(',');
-            var package = new PackageData { Name = parts[0], Url = parts[1], RemoteHash = parts[2] };
-            _packages.Add(package);
+            public string Name = "Empty";
+            public string Url = "Empty";
+            public string RemoteHash = "EmptyRemote";
+            public string LocalHash = "EmptyLocal";
+            public bool Installed = false;
         }
 
-        _ = LoadLocalData();
-    }
+        private const string kUrl =
+            "https://script.google.com/macros/s/AKfycbyTB22eh9Todbw66Xx6qk-pUb3jzBN8ysnPlaSwZESKFoYAGbE/exec";
 
-    void OnGUI()
-    {
-        void DrawPackage(PackageData package)
+        private const string kPrefsDataKey = "JACKSPARROT_PACKAGES";
+        private const string kPrefsTimeKey = "JACKSPARROT_PACKAGES_TIME";
+
+        private List<PackageData> _packages = new List<PackageData>();
+        private string _status = string.Empty;
+        private bool _loading = true;
+
+        [MenuItem("JackSParrot/Packages")]
+        private static void OpenWindow()
         {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label(package.Name, GUILayout.Width(200));
-            if (package.Installed)
+            JackSParrotPackageManager window = (JackSParrotPackageManager) GetWindow(typeof(JackSParrotPackageManager));
+            _ = window.Initialize();
+            window.Show();
+        }
+
+        private async Task Initialize(bool forced = false)
+        {
+            _loading = true;
+            if (!EditorPrefs.HasKey(kPrefsDataKey) || forced)
             {
-                if (package.RemoteHash.Equals(package.LocalHash))
+                await DownloadLatestConfig();
+            }
+            else
+            {
+                ParseData(EditorPrefs.GetString(kPrefsDataKey));
+            }
+        }
+
+        private void OnFocus()
+        {
+            _ = Initialize();
+        }
+
+        private void ParseData(string data)
+        {
+            _status = "Parsing received data";
+            _packages.Clear();
+            var lines = data.Split(';');
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                var package = new PackageData {Name = parts[0], Url = parts[1], RemoteHash = parts[2]};
+                _packages.Add(package);
+            }
+
+            _ = LoadLocalData();
+        }
+
+        private void OnGUI()
+        {
+            void DrawPackage(PackageData package)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(package.Name, GUILayout.Width(200));
+                if (package.Installed)
                 {
-                    GUILayout.Label("Up to date", GUILayout.Width(80));
+                    if (package.RemoteHash.Equals(package.LocalHash))
+                    {
+                        GUILayout.Label("Up to date", GUILayout.Width(80));
+                    }
+                    else if (GUILayout.Button("Update", GUILayout.Width(80)))
+                    {
+                        AddPackage(package);
+                    }
+
+                    if (GUILayout.Button("Remove", GUILayout.Width(80)))
+                    {
+                        RemovePackage(package);
+                    }
                 }
-                else if (GUILayout.Button("Update", GUILayout.Width(80)))
+                else if (GUILayout.Button("Install", GUILayout.Width(80)))
                 {
-                    _ = AddPackage(package);
+                    AddPackage(package);
                 }
 
-                if (GUILayout.Button("Remove", GUILayout.Width(80)))
+                GUILayout.EndHorizontal();
+            }
+
+            GUILayout.BeginVertical();
+            if (_loading)
+            {
+                GUILayout.Label(_status);
+            }
+            else
+            {
+                if (GUILayout.Button("Refresh"))
                 {
-                    _ = RemovePackage(package);
+                    _ = Initialize(true);
+                }
+
+                foreach (var packageData in _packages)
+                {
+                    DrawPackage(packageData);
                 }
             }
-            else if (GUILayout.Button("Install", GUILayout.Width(80)))
+
+            GUILayout.EndVertical();
+        }
+
+        private async Task DownloadLatestConfig()
+        {
+            _status = "Requesting package config to server";
+            var request = new UnityWebRequest(kUrl, "GET", new DownloadHandlerBuffer(), null);
+            var handler = request.SendWebRequest();
+            while (!handler.isDone)
             {
-                _ = AddPackage(package);
+                await Task.Yield();
             }
-            GUILayout.EndHorizontal();
-        }
 
-        GUILayout.BeginVertical();
-        if(_loading)
-        {
-            GUILayout.Label(_status);
-        }
-        else
-        {
-            if (GUILayout.Button("Refresh"))
+            if (!string.IsNullOrEmpty(request.error))
             {
-                Initialize(true);
+                _status = "Error: " + request.error;
+                Debug.LogError(request.error);
+                return;
             }
-            foreach (var packageData in _packages)
+
+            string data = request.downloadHandler.text;
+            EditorPrefs.SetString(kPrefsDataKey, data);
+            ParseData(data);
+        }
+
+        private async Task LoadLocalData()
+        {
+            _loading = true;
+            _status = "Loading local packages";
+            var listRequest = UnityEditor.PackageManager.Client.List(true, false);
+            while (!listRequest.IsCompleted)
             {
-                DrawPackage(packageData);
+                await Task.Yield();
             }
-        }
-        GUILayout.EndVertical();
-    }
 
-    void OnFocus()
-    {
-        Initialize();
-    }
-
-    async Task DownloadLatestConfig()
-    {
-        _status = "Requesting package config to server";
-        var request = new UnityWebRequest(kUrl, "GET", new DownloadHandlerBuffer(), null);
-        var handler = request.SendWebRequest();
-        while (!handler.isDone)
-        {
-            await Task.Delay(16);
-        }
-        ConfigDownloaded(request);
-    }
-
-    async Task LoadLocalData()
-    {
-        _loading = true;
-        _status = "Loading local packages";
-        var listRequest = UnityEditor.PackageManager.Client.List(true, false);
-        while (!listRequest.IsCompleted)
-        {
-            await Task.Delay(16);
-        }
-        foreach (var package in listRequest.Result)
-        {
-            var packageData = _packages.Find(p => p.Name.Equals(package.name));
-            if (packageData != null)
+            foreach (var package in listRequest.Result)
             {
-                packageData.LocalHash = package.git.hash;
-                packageData.Installed = true;
+                var packageData = _packages.Find(p => p.Name.Equals(package.name));
+                if (packageData != null)
+                {
+                    packageData.LocalHash = package.git.hash;
+                    packageData.Installed = true;
+                }
             }
-        }
-        _status = string.Empty;
-        _loading = false;
-    }
 
-    async Task RemovePackage(PackageData package)
-    {
-        _loading = true;
-        _status = "Removing package " + package.Name;
-        var request = UnityEditor.PackageManager.Client.Remove(package.Name);
-        while (!request.IsCompleted)
-        {
-            await Task.Delay(16);
+            _status = string.Empty;
+            _loading = false;
         }
-    }
 
-    async Task AddPackage(PackageData package)
-    {
-        _loading = true;
-        _status = "Installing package " + package.Name;
-        var addReq = UnityEditor.PackageManager.Client.Add(package.Url + '#' + package.RemoteHash);
-        while (!addReq.IsCompleted)
+        private void RemovePackage(PackageData package)
         {
-            await Task.Delay(16);
+            _loading = true;
+            _status = $"Removing package {package.Name}";
+            UnityEditor.PackageManager.Client.Remove(package.Name);
+        }
+
+        private void AddPackage(PackageData package)
+        {
+            _loading = true;
+            _status = $"Installing package {package.Name}";
+            UnityEditor.PackageManager.Client.Add($"{package.Url}#{package.RemoteHash}");
         }
     }
 }
